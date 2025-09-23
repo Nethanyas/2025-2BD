af89d05f4ac479686f963e4d577a24335e769b26
Nethanyas <nethanyasdomingosdasilva@gmail.com>
2025-09-23 17:51:28 -0300

    pagando aluno matriculado

--- sistema/excluir.php
+++ sistema/excluir.php
@@ -1,7 +1,12 @@
 <?php
     include 'conexao.php';
-    $id = $_POST['btExcluir'];
+    $id = $_POST['btnExcluir'];
 
+    // APAGAR MATRICULA SE EXISTIR
+        $sql = $pdo->prepare("DELETE FROM MATRICULAS WHERE ALUNOS_ID = ?");
+        $sql->execute([$id]);
+
+        // apagar aluno
     $sql = $pdo->prepare("DELETE FROM ALUNOS WHERE ID = ?");
     $sql->execute([$id]);
 
